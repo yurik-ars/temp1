409c6ebbc13934aaeed95cffaa5b817853a29fdc
yurikars <buddy.yurik@gmail.com>
2014-08-13 11:31:52 +0300

    new git place in temp1

---
+++ Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.test;
+
+/**
+ * Created by Yury_Arski on 7/17/2014.
+ */
+public class Solution {
+public static void main(String[]args){
+    String data=new String ("MAY 1 2012");
+    System.out.println(data);
+}
+}
